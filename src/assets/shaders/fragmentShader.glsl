cc8d2c02eb16a4f8e9731bb3eba13ae5ef2cc6c6
ben-dh3 <benjaminduffield1997@gmail.com>
2024-08-15 14:20:44 +0100

    add shaders

--- src/assets/shaders/fragmentShader.glsl
+++ src/assets/shaders/fragmentShader.glsl
@@ -1,13 +1,93 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float u_time;
+uniform vec2 u_mouse;
+uniform vec3 u_colorA;
+uniform vec3 u_colorB;
+uniform vec3 u_colorC;
+
 varying vec2 vUv;
 
-vec3 colorA = vec3(0.912,0.191,0.652);
-vec3 colorB = vec3(1.000,0.777,0.052);
+/**
+ * Fractal Brownian Motion
+ *
+ * Reference: https://thebookofshaders.com/13/
+ * 
+ * See also: https://iquilezles.org/articles/morenoise
+ *         : https://iquilezles.org/articles/warp
+ */
 
-void main() {
-  // "Normalizing" with an arbitrary value
-  // We'll see a cleaner technique later :)   
-  vec2 normalizedPixel = gl_FragCoord.xy/800.0;
-  vec3 color = mix(colorA, colorB, normalizedPixel.x);
+#define NUM_OCTAVES 10
 
-  gl_FragColor = vec4(color,1.0);
+// Get random value
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+// Get noise
+float noise(vec2 st)
+{
+    // Splited integer and float values.
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+    
+    float a = random(i + vec2(0.0, 0.0));
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+    
+    // -2.0f^3 + 3.0f^2
+    vec2 u = f * f * (3.0 - 2.0 * f);
+    
+    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
+// Fractal Brownian Motion
+//
+// Reduce amplitude multiplied by 0.5, and frequency multiplied by 2.
+float fbm(vec2 st)
+{
+	float v = 0.0;
+    float a = 0.5;
+    
+    for (int i = 0; i < NUM_OCTAVES; i++)
+    {
+    	v += a * noise(st);
+        st = st * 0.99;
+        a *= 0.69;
+    }
+    
+    return v;
+}
+// End of Fractal Brownian Motion Code
+
+void main()
+{
+    vec2 st = (vUv * 0.5);
+    
+    vec3 color = vec3(0.0);
+        
+    vec2 q = vec2(0.0);
+    q.x = fbm(st + vec2(0.0) + (0.15 * u_time));
+    q.y = fbm(st + vec2(1.0) + (0.15 * u_time));
+    
+    vec2 r = vec2(0.0);
+    r.y = fbm(st + (4.0 * q) + vec2(8.3, 2.8) + (0.15 * u_time) * (sin(u_mouse.y * 0.001)));
+    
+    vec2 s = vec2(0.0);
+    s.x = fbm(st + (4.0 * q) + vec2(1.7, 9.2) + (0.15 * u_time) * (sin(u_mouse.x * 0.001)));
+    
+    color = mix(color, u_colorA, clamp(length(q), 0.0, 1.0));
+    color = mix(color, u_colorB, clamp(length(r), 0.0, 1.0));
+    color = mix(color, u_colorC, clamp(length(s), 0.0, 1.0));
+
+    float f = fbm(st + 4.0 * r);
+    
+    float coef = (f * f * f + (0.6 * f * f) + (0.5 * f));
+    color *= coef;
+    
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/components/ShaderBackground.jsx
+++ src/components/ShaderBackground.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
-import { Canvas } from "@react-three/fiber";
-import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMemo, useRef, useCallback, useEffect } from "react";
+import { Vector2, Color } from "three";
 
 import vertexShader from "../assets/shaders/vertexShader.glsl";
 import fragmentShader from "../assets/shaders/fragmentShader.glsl";
@@ -8,13 +9,48 @@ import fragmentShader from "../assets/shaders/fragmentShader.glsl";
 const Fragment = () => {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
+  const mousePosition = useRef({ x: 0, y: 0 });
+
+  const updateMousePosition = useCallback((e) => {
+    mousePosition.current = { x: e.pageX, y: e.pageY };
+  }, []);
+
+  const uniforms = useMemo(
+    () => ({
+      u_time: {
+        value: 0.0,
+      },
+      u_mouse: { value: new Vector2(0, 0) },
+      u_colorA: { value: new Color("#7ED320") },
+      u_colorB: { value: new Color("#D0011B") },
+      u_colorC: { value: new Color("#4990E2") },
+    }), []
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", updateMousePosition, false);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition, false);
+    };
+  }, [updateMousePosition]);
+
+  useFrame((state) => {
+    const { clock } = state;
+
+    mesh.current.material.uniforms.u_time.value = clock.getElapsedTime();
+    mesh.current.material.uniforms.u_mouse.value = new Vector2(
+      mousePosition.current.x,
+      mousePosition.current.y
+    );
+  });
 
   return (
-    <mesh ref={mesh} position={[0, 0, 0]} scale={100.0}>
+    <mesh ref={mesh} position={[0, 0, 0]} scale={1.0}>
       <planeGeometry args={[1, 1, 32, 32]} />
       <shaderMaterial
         fragmentShader={fragmentShader}
         vertexShader={vertexShader}
+        uniforms={uniforms}
       />
     </mesh>
   );
@@ -22,7 +58,7 @@ const Fragment = () => {
 
 const ShaderBackground = () => {
   return (
-    <Canvas camera={{ position: [0.0, 0.0, 100.0] }}>
+    <Canvas camera={{ position: [0.0, 0.0, 1.0] }}>
       <Fragment />
     </Canvas>
   );
